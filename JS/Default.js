c55cc7d3900b89d1f7b5b94efd7949fed3c19319
jiangzhix <grasslandx@outlook.com>
2023-07-11 14:47:13 +0800

    updata

--- JS/Default.js
+++ JS/Default.js
@@ -1,5 +1,8 @@
 var starCount = 1500;
 var starArr = new Array();
+var hellotip = new String();
+
+hellotip = getNEMTtip()
 
 var Star = function () {
     this.x = windowWidth * Math.random();
@@ -39,5 +42,18 @@ function showStars() {
         context.fillText(starArr[n].text, starArr[n].x, starArr[n].y);
     }
 
-    setTimeout("showStars()", 100);
+    setTimeout("showStars()", 150);
+}
+
+function getNEMTtip() {
+    var date1 = '06/06/2024';
+    var date2 = new Date();
+    var date3 = date2.getTime() - new Date(date1).getTime();
+    var days = Math.floor(date3 / (24 * 3600 * 1000));
+    var leave1 = date3 % (24 * 3600 * 1000);
+    var hours = Math.floor(leave1 / (3600 * 1000));
+    var leave2 = leave1 % (3600 * 1000);
+    var minutes = Math.floor(leave2 / (60 * 1000));
+    var str = new String();
+    return "距离高考还有" + Math.abs(days) + "天" + Math.abs(hours) + "时" + Math.abs(minutes) + "分"
 }
--- Readme.md
+++ Readme.md
@@ -1,9 +1,18 @@
 # 星愿 - Dream - Project
 
-本项目仅作为 学习 使用。
+> 项目仅作为 学习 使用。
+
+星愿 - Dream - Project 是一个普通网页，提供给 所有人（目前是专门提供**高中学子**）许愿的地方。
+
+目前还未开放 投稿 地址，请耐心等待……
+
+## 待完成
+
+1. ~~ 星空背景 ~~
+
+2. 提示语 进行中……
+
 
 ## 借物表
 
-星空背景 ：
-
-> https://blog.csdn.net/zsms1314/article/details/124260830
+星空背景 ：https://blog.csdn.net/zsms1314/article/details/124260830
